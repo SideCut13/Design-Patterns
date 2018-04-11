d354ac8ff97d7d9e011c4b47417dd48d4feed37a
Anna Zdrojewska <anna_zdrojewska@interia.eu>
2018-04-11 23:19:30 +0200

    add adapter structural design pattern

--- src/adapter/class_adapter/MainClassAdapter.java
+++ src/adapter/class_adapter/MainClassAdapter.java
@@ -4,6 +4,7 @@ import adapter.MutantTurtle;
 
 public class MainClassAdapter {
     public static void main(String[] args) {
+
         MutantTurtle mutantTurtle = new TurtleClassAdapter();
         System.out.println("Turtle class adapter says: ");
         mutantTurtle.speak();
